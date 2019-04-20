da4cef365e13310d7da7115108b146fa56b7b6e8
Steven Scott <steven.scott@aggiemail.usu.edu>
2019-04-20 10:51:06 -0600

    High scores now displayable

--- scripts/client/highscores.js
+++ scripts/client/highscores.js
@@ -1,28 +1,59 @@
-MyGame.screens['high-scores'] = (function(game) {
+MyGame.screens['high-scores'] = (function (game) {
     'use strict';
-    var highScores = [];
-    
+    let highScores = {};
+    let highScoresList = [];
+
     function initialize() {
-        for(var i = 0; i < 4; i++)
-        {
-            highScores.push(localStorage.getItem(i));
-        }
         document.getElementById('id-high-scores-back').addEventListener(
             'click',
-            function() { game.showScreen('main-menu'); });
+            function () { game.showScreen('main-menu'); });
     }
-    
+
     function run() {
-        document.getElementById("one").innerHTML = highScores[0];
-        document.getElementById("two").innerHTML = highScores[1];
-        document.getElementById("three").innerHTML = highScores[2];
-        document.getElementById("four").innerHTML = highScores[3];
+        highScoresList = [];
+        let highScoresJSON = window.localStorage.getItem("highScores");
+        if (highScoresJSON) {
+            highScores = JSON.parse(highScoresJSON);
+        } else {
+            console.log('No high scores received');
+        }
+        console.log(highScores);
+
+        for (let clientId in highScores) {
+            highScores[clientId].inList = false; 
+        }
+        console.log(highScores); 
+        for (let i = 0; i < 5; i++) {
+            let displayText = "This could be you!";
+            let currentEntry = null; 
+            for (let clientId in highScores) {
+                let currentHighest = 0;
+                if (highScores[clientId]) {
+                    let score = highScores[clientId];
+                    if (score.score > currentHighest && !score.inList) {
+                        currentHighest = score.score;
+                        currentEntry = score; 
+                    }
+                }
+            }
+            if(currentEntry) {
+                currentEntry.inList = true; 
+                displayText = currentEntry.nickname + ' ' + currentEntry.score;
+            }
+            highScoresList.push(displayText);
+        }
+
+        document.getElementById("one").innerHTML = highScoresList[0];
+        document.getElementById("two").innerHTML = highScoresList[1];
+        document.getElementById("three").innerHTML = highScoresList[2];
+        document.getElementById("four").innerHTML = highScoresList[3];
+        document.getElementById("five").innerHTML = highScoresList[4];
         //
         // I know this is empty, there isn't anything to do.
     }
-    
+
     return {
-        initialize : initialize,
-        run : run
+        initialize: initialize,
+        run: run
     };
 }(MyGame.game));
